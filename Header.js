74aa4594594dfbf0dc4492c2d35a185ffec0cf23
Prasanth Remella <98454591+prasanth-surya@users.noreply.github.com>
2023-01-28 12:25:44 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="/images/troll-face.png" alt="" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+}
